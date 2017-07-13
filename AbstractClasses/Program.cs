fdc7b29370e6f57e8c0c2c82cd180aff113c3c69
Arvind Narang <arvindnarang1694@gmail.com>
2017-07-13 21:37:13 +0530

    pushing changes for abstract classes

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace AbstractClasses
 {
+
+    public abstract class AbsClass1
+    {
+        public abstract int EmpId { get; set; }
+
+        public void NonAbsMethod1()
+        {
+            Console.WriteLine("FYI Abstact classes can have NonAbstarct methods");
+        }
+
+        public abstract void AbsMethod1();
+        //{   commented because keep in mind that abstract methods cant have body in abstract class
+        //    Console.WriteLine("");
+        //}
+
+        //Abstract classes are basically used as BASE CLASS.
+        //An Abstarct class can not be sealed.
+    }
+
+    public class AbsClass2 :AbsClass1
+    {
+        public override int EmpId { get; set; }
+        public override void AbsMethod1() // to provide implementation of the abstarct class's abstract methods is mandatory.
+            // the only time child class doesnt have to provide the implementation is when it itself is also a Abstact class.
+        {
+            Console.WriteLine("in the child class it can have a body, remember using 'override' for that");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+          //  AbsClass1 obj = new AbsClass1();
+          // abstract classes are basically incomplete , So it doesnt make sense to create their object
+          // Abstract classes cant be instantiated.
+
+            AbsClass2 obj = new AbsClass2();
+            obj.NonAbsMethod1();
+            obj.AbsMethod1();
+            Console.ReadKey();
+
         }
     }
 }
